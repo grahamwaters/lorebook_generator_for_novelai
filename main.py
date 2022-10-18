8e869a798edaa3621f8f3d4a719d5434e5274810
Graham Waters <graham.waters.business@gmail.com>
2022-10-18 12:53:40 -0500

    adds secondary connections

--- main.py
+++ main.py
@@ -21,6 +21,49 @@ stop_words = set(stopwords.words('english'))
 
 # import the json file lorebook_example.lorebook
 
+"""
+    "Nikola Tesla",
+"Thomas Edison",
+"George Westinghouse",
+"Alexander Graham Bell",
+"Samuel Morse",
+"Benjamin Franklin",
+"Guglielmo Marconi",
+"London",
+"Paris",
+"New York",
+"Washington DC",
+"Berlin, Germany",
+"West Berlin",
+"East Berlin",
+"Hitler",
+"Joseph Stalin",
+"Franklin D. Roosevelt",
+"Harry S. Truman",
+"Queen Victoria",
+"Charles Dickens",
+"William Shakespeare",
+"Jules Verne",
+"Samuel Clemens",
+"Charles Darwin",
+"Albert Einstein",
+"Isaac Newton",
+"Leonardo da Vinci",
+"Captain James Cook",
+"Wardenclyffe Tower",
+"Famous Scientists",
+"Famous Paintings",
+"Mythical Creatures",
+"World War 1",
+"World War 2",
+"Western Front",
+"List_of_people_who_disappeared_mysteriously:_pre-1910",
+"List_of_people_who_disappeared_mysteriously:_1910-1949",
+"List_of_people_who_disappeared_mysteriously:_1950-1999",
+"List_of_people_who_disappeared_mysteriously:_2000-present",
+
+    _extended_summary_
+"""
 
 def preprocess(sent):
     sent = nltk.word_tokenize(sent)
@@ -99,21 +142,85 @@ def generate_entries_from_list(list_of_names):
 #     'Test 2',
 #     'Test 3'
 # ]
+# get all links on each of the entity pages
+from tqdm import tqdm
+import pandas as pd
+import wikipedia
 
 def main():
 
 
-    with open('starter.lorebook') as f:
+
+
+    # check those article pages for length (if they are too short, skip them)
+    # if they are long enough, and are not already in the list, add them to the list
+    list_of_names = pd.read_csv('characters.csv')
+    print(type(list_of_names))
+    list_of_names = list_of_names['Name'].tolist()
+    entries = []
+    entry_names = []
+    for name in tqdm(list_of_names):
+        if name != '':
+            try:
+                entry = wikipedia.search(name)[0] # get the first result from wikipedia, which is usually the most relevant
+                page = wikipedia.page(entry)
+                entry = page.content
+                links = page.links
+                print(f'Adding {name} to entries')
+                for link in tqdm(links):
+                    #print("Checking link ", link,end='')
+                    # for every 2nd degree page
+                    try:
+                        entry = wikipedia.search(link)[0] # get the first result from wikipedia, which is usually the most relevant
+                        page = wikipedia.page(entry)
+                        #!print(" length:", len(page.content))
+                        if link not in entry_names and page.content != '' and len(page.content) > 5000 and (name in page.content or page.content.find('born ')!=-1): # if the page is long enough and not already in the list, add it
+                            entries.append(page.content)
+                            entry_names.append(link)
+                            print(f' ==> Adding {link} to the list of entries')
+                    except:
+                        pass
+            except Exception as e:
+                print(e)
+                continue
+        df = pd.DataFrame(entry_names)
+        df.to_csv('characters.csv', index=False) # save the list of entries
+
+    # save entry_names to a csv
+    df = pd.DataFrame(entry_names)
+    df.to_csv('characters.csv', index=False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    with open('lorebook_generated.lorebook') as f:
         lore_dict = json.load(f)
 
     topics_list = []
     entry_keys = []
-    input_text = 'start'
-    while input_text != '':
-        input_text = input('Enter a topic: ')
-        topics_list.append(input_text)
+    # input_text = 'start'
+    # while input_text != '':
+    #     input_text = input('Enter a topic: ')
+    #     topics_list.append(input_text)
+    # read in the list of topics from the characters.csv file
+    topics_list = pd.read_csv('characters.csv')['Name'].tolist()
     assert(type(topics_list) == list) # make sure it's a list
     # entries, entry_names = generate_entries_from_list(lore_dict['people'])
+
+    # generate only the entries in topics_list that are not already in the lorebook
+    #existing_topics = [entry['name'] for entry in lore_dict['entries']]
+    #topics_list = [x for x in topics_list if x not in lore_dict]
+
     entries, entry_names, ids,entry_keywords = generate_entries_from_list(topics_list)
     # add the entries to the lorebook dictionary. All we have to change is the text, display name, create a random id, and add the keys (which are the words in the text). All other fields can be copied from the first entry.
 
@@ -121,9 +228,10 @@ def main():
     keys = []
     keys_dict = {}
     entry_id = 0
-    for entry in entries:
+    for entry in tqdm(entries):
+        print(f'Processing entry {entry[0:50]}...')
         keys = [] # reset the keys list, so we don't have duplicate keys
-        for word, tag in preprocess(entry):
+        for word, tag in tqdm(preprocess(entry)):
             if (tag == 'NNP' or tag == 'NNPS' or tag == 'CD') and word not in keys\
                 and word not in stop_words and len(word) > 2: # remove stop words, and numbers greater than 2020 (which are probably years)
                 try:
@@ -170,7 +278,7 @@ def main():
     context_config = {
             "prefix": "",
             "suffix": "\n",
-            "tokenBudget": 2048,
+            "tokenBudget": 100, # max 2048
             "reservedTokens": 0,
             "budgetPriority": 400,
             "trimDirection": "trimBottom",
@@ -183,9 +291,14 @@ def main():
 
     for i in range(len(entries)):
         # append blanks to lore_dict['entries'] to make room for the new entries
-        lore_dict['entries'].append({})
+        try:
+            lore_dict['entries'][i] = {}
+        except Exception as e:
+            print(e)
+            #lore_dict['entries'].append({'text': entries[i]})
+            lore_dict['entries'].append({})
 
-    for i in range(len(entries)):
+    for i in tqdm(range(len(entries))):
         # lore_dict > entries > text
         # add a new entry to the lorebook dictionary
         lore_dict['entries'][i]['text'] = str(entries[i])
